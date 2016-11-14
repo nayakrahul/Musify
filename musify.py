370625a5ab38199896e59c34d2718e6a3143c1a2
Rahul Nayak <rahulnayak@Rahuls-MacBook-Pro.local>
2016-11-14 22:19:33 +0530

    music downloader

---
+++ musify.py
@@ -0,0 +1,97 @@
+from google import search
+from threading import Thread
+from bs4 import BeautifulSoup
+from clint.textui import progress
+import urllib2
+import requests
+import sys
+import os
+import time
+import itertools
+
+
+flag = 0
+
+def progess():
+	global flag
+	sys.stdout.write("Searching...")
+	for c in itertools.cycle('/-\|'):
+		sys.stdout.write('\r\t\t' + c)
+		sys.stdout.flush()
+		time.sleep(0.2)
+		if flag :
+			break
+
+def play_song(file_path):
+	reply = raw_input("Do you want to play the song?(Y/N)")
+	if reply == 'Y' or reply == 'y':
+		cli = 'play ' + '"' + file_path + '"'
+		os.system(cli)
+	elif reply == 'N' or reply == 'n':
+		return
+	else:
+		play_song(file_path)
+
+def parse_url(url):
+	parse_url_components = url.split('//')
+	components = parse_url_components[1].split('/')
+	return components
+
+def download_song(download_url):
+	file_name = download_url.split('/')[-1]
+	music_path = os.path.expanduser('~')+"/Music"
+	file_path = os.path.join(music_path, file_name)
+	r = requests.get(download_url, stream=True)
+	with open(file_path, 'wb') as f:
+		global flag
+		flag = 1
+		sys.stdout.write("\t[DONE]\n")
+		total_length = int(r.headers.get('content-length'))
+		for chunk in progress.bar(r.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1): 
+			if chunk:
+				f.write(chunk)
+				f.flush()
+	return file_path, file_name
+
+def get_download_link(song_url):
+	response = urllib2.urlopen(song_url)
+	soup = BeautifulSoup(response,  "lxml")
+	# print soup.prettify()
+	kwargs={'class':'touch'}
+	for link in soup.find_all('a', **kwargs):
+		if link.find('Download in 128 kbps') != -1:
+			download_url = link.get('href')
+			return download_url
+	return
+
+def search_song(search_term):
+	search_term = search_term + " mp3 download mr jatt"
+	song_url = ''
+	for url in search(search_term, lang='en', stop=1):
+		components = parse_url(url)
+		if components[0] == 'mr-jatt.com' and components[1].find('download') != -1:
+			song_url = url
+			return song_url
+	global flag
+	flag = 1
+	sys.stdout.write("\tFailed !!\n")
+	sys.exit() 
+
+
+def start(search_term):
+	song_url = search_song(search_term)
+	download_url = get_download_link(song_url)
+	file_path, file_name = download_song(download_url)
+	sys.stdout.write(file_name + " saved to Music !!\n")
+	play_song(file_path)
+
+if __name__ == '__main__':
+	try:
+	   	t1 = Thread(target=progess, args=())
+	   	t1.start()
+	   	t2 = Thread(target=start, args=(sys.argv[1],))
+	   	t2.start()
+	except:
+	   print "Error: unable to start thread"	
+
+
---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo -e "\nmusify(){
+	python $PWD/musify.py \"\$1\"
+}" >> ~/.bash_profile
